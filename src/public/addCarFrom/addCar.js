79629078bfc06f5a0f0703bca20408c2d1f99f22
mahmoud Diab <mhmoddiab2@gmail.com>
2021-02-09 15:01:04 +0200

    add forms

---
+++ src/public/addCarFrom/addCar.js
@@ -0,0 +1,8 @@
+const output = document.getElementById("output");
+const carName = document.getElementById("name");
+const carModele = document.getElementById("model");
+const carCountry = document.getElementById("country");
+const carIcon = document.getElementById("icon");
+const form = document.getElementById("addCFrom");
+
+form.addEventListener("submit", () => {});
---
+++ src/public/addEmploye/employeForm.js
@@ -0,0 +1,7 @@
+const name = document.getElementById("name");
+const company = document.getElementById("company");
+const job = document.getElementById("job");
+const phoneNumber = document.getElementById("phone");
+const email = document.getElementById("email");
+const output = document.getElementById("errortext");
+const form = document.getElementById("addEForm");
